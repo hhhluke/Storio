14861ed7106a193df4d802e35d1444f9c2e3f150
Luke <luke@LukedeMacBook-Air.local>
2025-07-31 11:34:53 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isGithubPages = process.env.NODE_ENV === 'production';
+const Prefix = '/Storio'
+const nextConfig = {
+    output: 'export',
+    assetPrefix: isGithubPages ? `${Prefix}/` : '',
+    basePath: isGithubPages ? Prefix : '',
+    images: {
+      unoptimized: true,
+    },
+}
 
 module.exports = nextConfig
